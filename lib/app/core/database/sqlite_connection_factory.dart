badd1922180ed5f007972781ea842d09b28164ba
Juan Veras <juan@vvssistemas.com.br>
2023-08-22 23:01:52 -0300

    Migrations configuradas

--- lib/app/core/database/sqlite_connection_factory.dart
+++ lib/app/core/database/sqlite_connection_factory.dart
@@ -1,6 +1,7 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:synchronized/synchronized.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_migration_factory.dart';
 
 class SqliteConnectionFactory {
   static const _VERSION = 1;
@@ -27,10 +28,12 @@ class SqliteConnectionFactory {
       await _lock.synchronized(() async {
         // ignore: prefer_conditional_assignment
         if (_db == null) {
-          _db = await openDatabase(
-            databasePathFinal,
-            version: _VERSION,
-          );
+          _db = await openDatabase(databasePathFinal,
+              version: _VERSION,
+              onConfigure: _onConfigure,
+              onCreate: _onCreate,
+              onUpgrade: _onUpgrade,
+              onDowngrade: _onDowngrade);
         }
       });
     }
@@ -41,4 +44,32 @@ class SqliteConnectionFactory {
     _db?.close();
     _db == null;
   }
+
+  Future<void> _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    final batch = db.batch();
+
+    final migrations = SqliteMigrationFactory().getCreateMigration();
+    for (var migration in migrations) {
+      migration.create(batch);
+    }
+
+    batch.commit();
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int version) async {
+    final batch = db.batch();
+
+    final migrations = SqliteMigrationFactory().getUpgradeMigration(oldVersion);
+    for (var migration in migrations) {
+      migration.update(batch);
+    }
+
+    batch.commit();
+  }
+
+  Future<void> _onDowngrade(Database db, int oldVersion, int version) async {}
 }
