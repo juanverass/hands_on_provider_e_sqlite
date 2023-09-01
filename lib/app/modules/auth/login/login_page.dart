bd5c3760e92dbffcebcd61018823eac1ce4fe9a2
Juan Veras <juan@vvssistemas.com.br>
2023-09-01 15:42:41 -0300

    Finalizando customização do textFormField

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:todo_list_provider/app/core/widgets/todo_list_field.dart';
 import 'package:todo_list_provider/app/core/widgets/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
@@ -32,11 +33,16 @@ class LoginPage extends StatelessWidget {
                       child: Form(
                           child: Column(
                         children: [
-                          TextFormField(),
+                           TodoListField(
+                            label: 'E-mail',
+                          ),
                           const SizedBox(
                             height: 20,
                           ),
-                          TextFormField(),
+                           TodoListField(
+                            label: 'Senha',
+                            obscureText: true,
+                          ),
                           const SizedBox(
                             height: 10,
                           ),
@@ -98,7 +104,10 @@ class LoginPage extends StatelessWidget {
                               children: [
                                 const Text('Não tem conta?'),
                                 TextButton(
-                                  onPressed: () {},
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamed('/register');
+                                  },
                                   child: const Text('Cadastre-se'),
                                 )
                               ],
